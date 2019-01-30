7e660e4060c5d753600ab577ba978bec23d9dc41
Intaek Shin <sit921212@gmail.com>
2019-01-30 08:50:34 +0000

    D21 | 190130 PM | 1:N Relation

--- django/crud-plus/posts/views.py
+++ django/crud-plus/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Post
+from .models import Post, Comment
 # 현재 폴더안에 있는 models파일을 그리고 그 중에  Post class를 임포트 한다.
 # Create your views here.
 # views.py -> urls.py -> templates
@@ -51,226 +51,22 @@ def update(request, post_id):
     post.save()
     return redirect('posts:detail', post.pk)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+def comments_create(request, post_id):
+    # 댓글을 달 게시물
+    post = Post.objects.get(pk=post_id)
+    
+    # form에서 넘어온 댓글 내용
+    content = request.POST.get('content')
+    
+    # 댓글 생성 및 저장
+    comment = Comment(post=post, content=content) #인스턴스 생성.
+    comment.save()
+    
+    return redirect('posts:detail', post.pk)
+    
+def comments_delete(request, post_id, comment_id):
+    comment = Comment.objects.get(pk=comment_id)
+    comment.delete()
+    return redirect('posts:detail', post_id)
     
     
---
+++ workshop/d20/surveys/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from .models import Question, Choice
+
+# Create your views here.
